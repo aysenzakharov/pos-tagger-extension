b27d6b37ec7c2b4126679fa84e1aa3197d412480
Aysen Zakharov <zeinell69@gmail.com>
2024-11-13 22:00:17 +0300

    added browser.runtime.onInstalled

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,22 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  // console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason === browser.runtime.OnInstalledReason.INSTALL) {
+      // Code to be executed on first install
+      // eg. open a tab with a url
+      browser.tabs.create({
+        url: "https://aysenzakharov.github.io/welcome",
+      });
+    } else if (details.reason === browser.runtime.OnInstalledReason.UPDATE) {
+      // When extension is updated
+    } else if (
+      details.reason === browser.runtime.OnInstalledReason.CHROME_UPDATE
+    ) {
+      // When browser is updated
+    } else if (
+      details.reason === browser.runtime.OnInstalledReason.SHARED_MODULE_UPDATE
+    ) {
+      // When a shared module is updated
+    }
+  });
 });
