8611f5fccc7e2c87e9b0892e1bbd328cb548ddbf
Aysen Zakharov <zeinell69@gmail.com>
2024-11-13 22:00:14 +0300

    upd

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   manifest: {
+    name: 'Part of Speech Identifier',
     permissions: ['storage', 'tabs', "activeTab"],
     host_permissions: [
       "https://part-of-speech-tool.info/*",
